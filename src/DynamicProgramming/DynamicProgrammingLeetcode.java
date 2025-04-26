e43cd9a7c2c7db3441d9a78d540dc6e00dadacc9
Aditya Simant <aditya@metapointer.com>
2025-04-26 15:17:58 +0530

    Dp

--- src/DynamicProgramming/DynamicProgrammingLeetcode.java
+++ src/DynamicProgramming/DynamicProgrammingLeetcode.java
@@ -1,13 +1,36 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class DynamicProgrammingLeetcode {
     public static void main(String[] args) {
-        MainSolution sol = new MainSolution();
-        int[] arr = {1,100,1,1,1,100,1,1,100,1};
-        int ans = sol.minCostClimbingStairs(arr);
-        System.out.println(ans);
+
+        /* Your class should be named Solution.
+         * Read input as specified in the question.
+         * Print output as specified in the question.
+         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] memo = new int[n+1];
+//        Arrays.fill(memo, -1);
+//        System.out.println(findFibo(n, memo));
+        memo[0] = 0;
+        memo[1] = 1;
+        for (int i=2; i<=n; i++){
+            memo[i] = memo[i-1] + memo[i-2];
+        }
+        System.out.println(memo[n]);
+    }
+    public static int findFibo(int n, int[] memo){
+        if (n<2) return n;
+        if (memo[n] != -1) return memo[n];
+
+        int ans = findFibo(n-1, memo) + findFibo(n-2, memo);
+        memo[n] = ans;
+
+        return ans;
     }
 }
 
@@ -27,22 +50,22 @@ class MainSolution{
         return recurSolution;
     }
 
-    public int minCostClimbingStairs(int[] cost) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        return minCostRecurr(cost.length, cost, map);
-    }
-
-    private static int minCostRecurr(int n,int[] cost, HashMap<Integer, Integer> map){
-        if (map.containsKey(n)) return map.get(n);
-        if (n == 1 || n==0) return 0;
-        int oneStep = minCostRecurr(n-1, cost, map) + cost[n-1];
-        int secondStep = minCostRecurr(n-2, cost, map) + cost[n-2];
-
-        int result = Math.min(oneStep, secondStep);
-        map.put(n, result);
-
-        return result;
-    }
+//    public int minCostClimbingStairs(int[] cost) {
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        return minCostRecurr(cost.length, cost, map);
+//    }
+//
+//    private static int minCostRecurr(int n,int[] cost, HashMap<Integer, Integer> map){
+//        if (map.containsKey(n)) return map.get(n);
+//        if (n == 1 || n==0) return 0;
+//        int oneStep = minCostRecurr(n-1, cost, map) + cost[n-1];
+//        int secondStep = minCostRecurr(n-2, cost, map) + cost[n-2];
+//
+//        int result = Math.min(oneStep, secondStep);
+//        map.put(n, result);
+//
+//        return result;
+//    }
 
     // 509. Fibonacci Number
     static HashMap<Integer, Integer> map = new HashMap<>();
@@ -83,4 +106,51 @@ class MainSolution{
         memo.put(n, ans);
         return ans;
     }
+
+    static int knapsack(int[] weight, int[] value, int n, int maxWeight) {
+        Integer[][] dp = new Integer[n][maxWeight + 1];
+        return findMaxWeight(weight, value, n, maxWeight, 0, 0, dp);
+    }
+
+    static int findMaxWeight(int[] weight, int[] value, int n, int maxWeight, int index, int currWeight, Integer[][] dp) {
+        if (index == n) return 0;
+
+        if (dp[index][currWeight] != null) return dp[index][currWeight];
+
+        int noPick = findMaxWeight(weight, value, n, maxWeight, index + 1, currWeight, dp);
+        int pick = 0;
+        if (currWeight + weight[index] <= maxWeight) {
+            pick = value[index] + findMaxWeight(weight, value, n, maxWeight, index + 1, currWeight + weight[index], dp);
+        }
+
+        return dp[index][currWeight] = Math.max(pick, noPick);
+    }
+
+
+    public int minCostClimbingStairs(int[] cost) {
+        int[] memo = new int[cost.length+1];
+        Arrays.fill(memo, -1);
+        int jump1 = findMinCostRecur(cost,0, 0, memo);
+        int jump2 = findMinCostRecur(cost,1, 0, memo);
+
+        return Math.min(jump2, jump1);
+    }
+
+    int findMinCostRecur(int[] cost, int index, int jumpSum, int[] memo){
+        int n = cost.length;
+        if (index>=n) return jumpSum;
+        if(memo[index] != -1) return memo[index];
+
+        int jump1 = findMinCostRecur(cost, index+1, jumpSum+cost[index], memo);
+        int jump2 = findMinCostRecur(cost, index+2, jumpSum+cost[index], memo);
+
+        memo[index] = Math.min(jump2, jump1);
+
+        return memo[index];
+    }
+
+
+
+
+
 }
